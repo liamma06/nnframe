5e001d13de8e1e26de837528fe30f425ca38d626
liamma06 <liam.jbr.ma@gmail.com>
2026-07-07 14:57:52 -0400

    reorder for clarity

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -2,24 +2,10 @@
 #include <cassert> //checks 
 #include <numeric> 
 
-//hlper stride func 
-size_t Tensor::compute_strides() {
-    strides_.resize(shape_.size()); //match shape size
-    size_t stride = 1;
-    for (int i = shape_.size() - 1; i >= 0; i--) {
-        strides_[i] = stride; //save stride_ {..., ...} 
-        stride *= shape_[i];
-    }
-
-    return stride;  //total
-}
-
 Tensor::Tensor(std::vector<size_t> shape, scalar_t fill){
     shape_ = shape;
     offset_ = 0;
-
-    //shape - > stride 
-    size_t stride = compute_strides(); 
+    size_t stride = compute_strides(); //shape - > stride 
 
     //allocate memory 
     data_ = std::make_shared<std::vector<scalar_t>>(stride, fill); //ptr to vector fill
@@ -34,4 +20,15 @@ Tensor::Tensor(std::vector<size_t> shape, std::vector<scalar_t> data){
     assert(data.size() == stride && "Data size does not match shape size");
 
     data_ = std::make_shared<std::vector<scalar_t>>(data); //direct input data
+}
+
+//hlper stride func 
+size_t Tensor::compute_strides() {
+    strides_.resize(shape_.size()); //match shape size
+    size_t stride = 1;
+    for (int i = shape_.size() - 1; i >= 0; i--) {
+        strides_[i] = stride; //save stride_ {..., ...} 
+        stride *= shape_[i];
+    }
+    return stride;  //total
 }
