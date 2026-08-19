73be52262a32673768ccbfdff91746d3f5b3402a
liamma06 <109089492+liamma06@users.noreply.github.com>
2026-08-19 14:42:00 -0400

    cuda paged attention

--- include/infer/paged_kv_cache.h
+++ include/infer/paged_kv_cache.h
@@ -11,9 +11,10 @@ class PagedKVCache {
         size_t block_size_; //# of tokens per block
         size_t capacity_seq_len_;
         size_t curr_len_ = 0;
+        Device device_;
 
     public:
-            PagedKVCache(size_t num_heads, size_t head_dim, size_t max_seq_len, size_t block_size = 16);
+            PagedKVCache(size_t num_heads, size_t head_dim, size_t max_seq_len, size_t block_size = 16, Device device = Device::CPU);
 
             void append(const TensorPtr& new_k, const TensorPtr& new_v);
             TensorPtr get_k() const;
--- src/KVCache/PagedKVCache.cpp
+++ src/KVCache/PagedKVCache.cpp
@@ -2,13 +2,39 @@
 #include <cassert>
 #include <stdexcept>
 
-PagedKVCache::PagedKVCache(size_t num_heads, size_t head_dim, size_t max_seq_len, size_t block_size){
+#if defined(NNFRAME_WITH_CUDA)
+    #include "cuda/matmul_cuda.cuh"
+#endif
+
+PagedKVCache::PagedKVCache(size_t num_heads, size_t head_dim, size_t max_seq_len, size_t block_size, Device device){
     num_heads_ = num_heads;
     head_dim_ = head_dim;
     block_size_ = block_size;
+    device_ = device;
 
     //to the nearest block size
     capacity_seq_len_ = ((max_seq_len + block_size - 1) / block_size) * block_size; 
+
+    #ifdef NNFRAME_WITH_CUDA
+    if (device_ == Device::CUDA){
+        scalar_t* d_k = nullptr;
+        scalar_t* d_v = nullptr;
+
+        size_t size_kv = num_heads_ * capacity_seq_len_ * head_dim_ * sizeof(scalar_t);
+
+        CUDA_CHECK(cudaMallocAsync(&d_k, size_kv, 0));
+        CUDA_CHECK(cudaMallocAsync(&d_v, size_kv, 0));
+
+        std::vector<size_t> shape = {num_heads_, capacity_seq_len_, head_dim_};
+        std::vector<size_t> strides = {capacity_seq_len_ * head_dim_, head_dim_, 1};
+
+        cache_k_ = Tensor::from_device_ptr(d_k, shape, strides);
+        cache_v_ = Tensor::from_device_ptr(d_v, shape, strides);
+
+        return;
+    }
+    #endif
+
     cache_k_ = Tensor::create({num_heads_, capacity_seq_len_, head_dim_});
     cache_v_ = Tensor::create({num_heads_, capacity_seq_len_, head_dim_});
 }
@@ -22,7 +48,23 @@ PagedKVCache::PagedKVCache(size_t num_heads, size_t head_dim, size_t max_seq_len
         throw std::runtime_error("eviction not done yet");
     }
 
-    //copy new
+    #ifdef NNFRAME_WITH_CUDA
+        //similar to the normal KVcahce imp but instead of copy old data just input 
+        if (device_ == Device::CUDA){
+            for (size_t h = 0; h < num_heads_; h++){
+                size_t new_data_offset_location = h * new_seq_len * head_dim_; // offset from input
+                size_t new_data_offset_landing = h * capacity_seq_len_ * head_dim_ + curr_len_ * head_dim_;
+
+                CUDA_CHECK(cudaMemcpyAsync(cache_k_->mutable_device_data() + new_data_offset_landing, new_k->device_data() + new_data_offset_location, new_seq_len * head_dim_ * sizeof(scalar_t), cudaMemcpyDeviceToDevice));
+                CUDA_CHECK(cudaMemcpyAsync(cache_v_->mutable_device_data() + new_data_offset_landing, new_v->device_data() + new_data_offset_location, new_seq_len * head_dim_ * sizeof(scalar_t), cudaMemcpyDeviceToDevice));
+            }
+            
+            curr_len_ = new_total_len;
+            return;
+        }
+    #endif
+
+    //copy new into the empty space 
     for (size_t h = 0; h < num_heads_; h++){
         for (size_t i = 0; i < new_seq_len; i++){
             for (size_t j = 0; j < head_dim_; j++){
@@ -36,7 +78,25 @@ PagedKVCache::PagedKVCache(size_t num_heads, size_t head_dim, size_t max_seq_len
 
 }
 
+
 TensorPtr PagedKVCache::get_k() const {
+    #ifdef NNFRAME_WITH_CUDA
+        if (device_ == Device::CUDA){
+            scalar_t* d_out = nullptr;
+            CUDA_CHECK(cudaMallocAsync(&d_out, num_heads_ * curr_len_ * head_dim_ * sizeof(scalar_t), 0));
+
+            for (size_t h = 0; h < num_heads_; h++){
+                size_t src_offset = h * capacity_seq_len_ * head_dim_;
+                size_t dst_offset = h * curr_len_ * head_dim_;
+                CUDA_CHECK(cudaMemcpyAsync(d_out + dst_offset, cache_k_->device_data() + src_offset, curr_len_ * head_dim_ * sizeof(scalar_t), cudaMemcpyDeviceToDevice));
+            }
+
+            std::vector<size_t> shape = {num_heads_, curr_len_, head_dim_};
+            std::vector<size_t> strides = {curr_len_ * head_dim_, head_dim_, 1};
+            return Tensor::from_device_ptr(d_out, shape, strides);
+        }
+    #endif
+
     auto valid_k = Tensor::create({num_heads_, curr_len_, head_dim_});
     for (size_t h = 0; h < num_heads_; h++){
         for (size_t i = 0; i < curr_len_; i++){
@@ -49,6 +109,24 @@ TensorPtr PagedKVCache::get_k() const {
 }
 
 TensorPtr PagedKVCache::get_v() const {
+    #ifdef NNFRAME_WITH_CUDA
+        if (device_ == Device::CUDA){
+            scalar_t* d_out = nullptr;
+            CUDA_CHECK(cudaMallocAsync(&d_out, num_heads_ * curr_len_ * head_dim_ * sizeof(scalar_t), 0));
+
+            for (size_t h = 0; h < num_heads_; h++){
+                size_t src_offset = h * capacity_seq_len_ * head_dim_;
+                size_t dst_offset = h * curr_len_ * head_dim_;
+                CUDA_CHECK(cudaMemcpyAsync(d_out + dst_offset, cache_v_->device_data() + src_offset,
+                                            curr_len_ * head_dim_ * sizeof(scalar_t), cudaMemcpyDeviceToDevice));
+            }
+
+            std::vector<size_t> shape = {num_heads_, curr_len_, head_dim_};
+            std::vector<size_t> strides = {curr_len_ * head_dim_, head_dim_, 1};
+            return Tensor::from_device_ptr(d_out, shape, strides);
+        }
+    #endif
+
     auto valid_v = Tensor::create({num_heads_, curr_len_, head_dim_});
     for (size_t h = 0; h < num_heads_; h++){
         for (size_t i = 0; i < curr_len_; i++){
